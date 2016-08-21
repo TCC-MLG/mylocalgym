90356d2eabbbf5f09bbec8eb326c1ab055bb26d7
matheus <mathgunz@gmail.com>
2016-08-21 12:50:01 -0300

    comunicação de tabelas

---
+++ src/main/java/br/com/gym/mylocalgym/main/Teste.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gym.mylocalgym.main;
+
+import br.com.gym.mylocalgym.configuration.HibernateUtil;
+import entities.Cliente;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Teste {
+
+    public void main() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Matheus siqueira");
+        
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Integer inte = (Integer) session.save(cliente);
+        
+        System.out.println(""+inte);
+
+    }
+
+}
